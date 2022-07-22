76dcfc209c77f8ddc036261868d56fdcce7af173
Hajin153 <hajin153@gmail.com>
2022-07-22 17:42:32 +0900

    모달 작업중

--- js/index.js
+++ js/index.js
@@ -23,6 +23,19 @@ $(function() {
         }
     })
 
+    //percent
+    $('.count').each(function() {
+        $(this).prop('Counter',0).animate({
+            Counter: $(this).text()
+        }, {
+            duration: 8000,
+            easing: 'swing',
+            step: function (now) {
+                $(this).text(Math.ceil(now) + "%");
+            }
+        });
+    });
+
     //hello p
     $(".hello-txt").hover(function() {
         $(".hello-txt").animate({
@@ -38,64 +51,46 @@ $(function() {
     //green ball
     // $(".green-ball").css($(window).width() / 2  - $(".green-ball").width()/2 + "px");
     $(window).scroll(function() { //문서가 스크롤되면, 메뉴의 위치 설정
-        var t = $(this).scrollTop() ; // 수직 스크롤의 위치값을 더해 메뉴 위치 설정
-        $(".green-ball").stop().animate({left:t}, 1000);
+        var t = $(this).scrollTop() + ($(this).width() / 6) ; // 수직 스크롤의 위치값을 더해 메뉴 위치 설정
+        $(".green-ball").stop().animate({left:t}, 3000);
     });
+
+    
 
     //orange ball
-    // $(window).scroll(function() { //문서가 스크롤되면, 메뉴의 위치 설정
-    //     var t = $(this).scrollTop() + ($(this).width() / 2 - $(".orange-ball").width() / 2);; // 수직 스크롤의 위치값을 더해 메뉴 위치 설정
-    //     $(".orange-ball").stop().animate({left:t}, 1000);
-    // });
+    $(window).scroll(function() { //문서가 스크롤되면, 메뉴의 위치 설정
+        var t = $(this).scrollTop() - 900  // 수직 스크롤의 위치값을 더해 메뉴 위치 설정
+        $(".orange-ball").stop().animate({left:-t}, 3000);
+    });
 
-    // var window_width = $(window).width() - $('.green-ball').width();
-    // var document_height = $(document).height() - $(window).height();
-    // $(".green-ball").css("top",$(window).height() / 2  - $(".green-ball").height()/2 + "px");
+    //green ball-2
+    $(window).scroll(function() { //문서가 스크롤되면, 메뉴의 위치 설정
+        var t = $(this).scrollTop() - 2000  // 수직 스크롤의 위치값을 더해 메뉴 위치 설정
+        $(".green-ball-2").stop().animate({left:t}, 3000);
+    });
 
-    // $(window).scroll(function () {
-    //     var scroll_position = $(window).scrollTop();
-    //     var object_position_left = window_width * (scroll_position / document_height);
-    //     $('.green-ball').css({
-    //         'left': object_position_left
-    //     });
-    // });
+
+    //portfolio slide
+    $("#arrow").click(function() {
+        $("#slider").append($("#slider > div:first-child")).css({left:"-936px"}).animate({left:0}, 500);
+    });
+
+    $("#arrow-1").click(function() {
+        $("#slider-1").prepend($("#slider-1 > div:last-child")).css({left:"-936px"}).animate({left:0}, 500);
+    });
+
+    $("#arrow-2").click(function() {
+        $("#slider-2").prepend($("#slider-2 > div:last-child")).css({left:"-936px"}).animate({left:0}, 500);
+    });
+
+    //my skill modal
     
+    $('#web-box').click(function() {
+        $('#myModal').addClass("active");
+    })
 
-    // var window_width = $(window).width() - $('.orange-ball').width();
-    // var document_height = $(document).height() - $(window).height();
-    //     $(window).scroll(function () {
-    //         var scroll_position = $(window).scrollTop();
-    //         var object_position_left = window_width * (scroll_position / document_height);
-    //         $('.orange-ball').css({
-    //             'left': object_position_left
-    //         });
-    //     });
+    $('.close').click(function() {
+        $('#myModal').removeClass("active");
+    })
 
-    //circle progress bar
-    var progressBarOptions = {
-        starAngle: -1.55,
-        size: 200,
-        value: 0.75,
-        fill: {
-            color: '#ffa500'
-        }
-    }
-    
-    $('.orange').circleProgress(progressBarOptions).on('circle-animation-progress', function(event, progress, stepValue) {
-        $(this).find('strong').text(String(stepValue.toFixed(2)).substr(1));
-    });
-
-    $('#c-a').circleProgress({
-        value : 0.25,
-        fill: {
-            color: '#FF0000'
-        }
-    });
-
-    $('#c-b').circleProgress({
-        value : 0.92,
-        fill: {
-            color: '#008000'
-        }
-    });
 })
