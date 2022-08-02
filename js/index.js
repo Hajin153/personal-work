0543615d2ed83d41f458433e67845539ed7599b1
Your Name <hajin153@gmail.com>
2022-08-02 17:06:53 +0900

    mobile-menu, top btn

--- js/index.js
+++ js/index.js
@@ -49,30 +49,27 @@ $(function() {
 
 
     //green ball
-    // $(".green-ball").css($(window).width() / 2  - $(".green-ball").width()/2 + "px");
-    $(window).scroll(function() { //문서가 스크롤되면, 메뉴의 위치 설정
-        var t = $(this).scrollTop() + ($(this).width() / 6) ; // 수직 스크롤의 위치값을 더해 메뉴 위치 설정
+    $(window).scroll(function() { 
+        var t = $(this).scrollTop() + ($(this).width() / 6) ;
         $(".green-ball").stop().animate({left:t}, 3000);
     });
 
-    
-
     //orange ball
-    $(window).scroll(function() { //문서가 스크롤되면, 메뉴의 위치 설정
-        var t = $(this).scrollTop() - 900  // 수직 스크롤의 위치값을 더해 메뉴 위치 설정
+    $(window).scroll(function() { 
+        var t = $(this).scrollTop() - 900  
         $(".orange-ball").stop().animate({left:-t}, 3000);
     });
 
     //green ball-2
-    $(window).scroll(function() { //문서가 스크롤되면, 메뉴의 위치 설정
-        var t = $(this).scrollTop() - 2000  // 수직 스크롤의 위치값을 더해 메뉴 위치 설정
+    $(window).scroll(function() { 
+        var t = $(this).scrollTop() - 2000 
         $(".green-ball-2").stop().animate({left:t}, 3000);
     });
 
 
     //portfolio slide
     $("#arrow").click(function() {
-        $("#slider").append($("#slider > div:first-child")).css({left:"-936px"}).animate({left:0}, 500);
+        $("#slider").prepend($("#slider > div:last-child")).css({left:"-936px"}).animate({left:0}, 500);
     });
 
     $("#arrow-1").click(function() {
@@ -83,14 +80,60 @@ $(function() {
         $("#slider-2").prepend($("#slider-2 > div:last-child")).css({left:"-936px"}).animate({left:0}, 500);
     });
 
-    //my skill modal
+
+   // 모달창
+   var modals = document.getElementsByClassName("modal");
+    var btns = document.getElementsByClassName("web-box");
+    var spanes = document.getElementsByClassName("close");
+    var funcs = [];
+ 
+    function Modal(num) {
+    return function() {
+        btns[num].onclick =  function() {
+            modals[num].style.display = "block";
+            console.log(num);
+        };
     
-    $('#web-box').click(function() {
-        $('#myModal').addClass("active");
+        spanes[num].onclick = function() {
+            modals[num].style.display = "none";
+        };
+    };
+    }
+    for(var i = 0; i < btns.length; i++) {
+    funcs[i] = Modal(i);
+    }
+    for(var j = 0; j < btns.length; j++) {
+    funcs[j]();
+    }
+    window.onclick = function(event) {
+    if (event.target.className == "modal") {
+        event.target.style.display = "none";
+    }
+    };
+
+
+    //메뉴 부드럽게 이동
+    var gnbA = $(".gnb>li>a");
+    gnbA.click(function() {
+        var target = $(this).attr("href"); 
+        $("html").animate({scrollTop:$(target).offset().top},1000); 
+    });
+
+    //top btn
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 200) {
+        $("#top-bt").fadeIn("fast");		
+        } else {
+        $("#top-bt").fadeOut("fast");		
+        }
+    });
+    $("#top-bt").click(function() {
+        $("html").animate({scrollTop:0}, 500);
     })
 
-    $('.close').click(function() {
-        $('#myModal').removeClass("active");
+    //mobile-menu
+    $(".mobile-menu").click(function() {
+        $(".orange-stroke").slideToggle();
     })
-
 })
+
